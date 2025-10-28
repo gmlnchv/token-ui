56d73d4506848f5db4f047cc98340b5640a05e34
Georgy Malanichev <georgy@malanichev.com>
2025-10-28 19:50:43 +1300

    Integrate Fumadocs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { RootProvider } from 'fumadocs-ui/provider/next'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans text-foreground`}
       >
-        {children}
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   )
