21d8280bb32e029e43da9dce29654f14451ad673
Georgy Malanichev <georgy@malanichev.com>
2026-01-01 21:47:25 +1300

    Use fumadocs HomeLayout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,17 @@
-import type { ReactNode } from 'react'
 import { HomeLayout } from 'fumadocs-ui/layouts/home'
+import Image from 'next/image'
+import type { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <HomeLayout
       nav={{
-        title: 'Token UI',
+        title: (
+          <div className="flex items-center gap-2">
+            <Image src="/logo.svg" alt="Token UI Logo" width={20} height={20} className="size-5" />
+            <span>Token UI</span>
+          </div>
+        ),
         url: '/',
       }}
       links={[
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <main className="flex flex-1 flex-col">
       <div className="container mx-auto flex flex-col items-center gap-4 py-8 text-center md:py-16 lg:py-20 xl:gap-6">
         <h1 className="max-w-4xl text-balance font-semibold text-5xl text-primary tracking-tighter sm:text-7xl md:text-7xl lg:text-7xl">
-          Component library for Design System Documentation
-        </h1>
+          Component library for Design System Documentation</h1>
 
         <p className="mx-auto max-w-lg text-balance text-center text-base text-muted-foreground tracking-tight md:text-lg">
           Token UI is a custom registry of composable primitives for documenting your design
